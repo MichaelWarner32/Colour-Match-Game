43508a3018a1e8c45dd36fc3c26d783ae774f4d8
MichaelWarner32 <warner.mike32@gmail.com>
2020-08-30 17:30:11 +0000

    Declared initial variables I will use throughout the main.js file.

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var power = "off";
+var oneLife = "off";
+var run = false;
+var memory = [];
+var memoryCount = 0;
+var user = [];
+var userCount = 0;
+var levelCount = 1;
+var tempColor;
+var runningMemory;
+var matching = true;
+var speed;
+
+function buttonColor(id, color) {
+    this.id =id;
+    this.color = color;
+}
+var blue = new buttonColor(1, "blue");
+var green = new buttonColor(2, "green");
+var red = new buttonColor(3, "red");
+var yellow = new buttonColor(4, "yellow");
+
