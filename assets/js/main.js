b7ab50535449608245aa752420588925098431c4
MichaelWarner32 <warner.mike32@gmail.com>
2020-08-30 19:27:04 +0000

    Added opacity value to "textDisplay" when power button is off. Also added on click features for the power switch.

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,18 +20,33 @@ var green = new buttonColor(2, "green");
 var red = new buttonColor(3, "red");
 var yellow = new buttonColor(4, "yellow");
 
+//--When clicking power button--//
+
 $("#powerSwitch").on("click", function() {
-    if (power == "off") {
+if (power == "off") {
     $("#onButton").css("visibility", "visible");
     $("#offButton").css("visibility", "hidden");
     $("#textDisplay").css("opacity", "1");
     power = "on";
     }
-    else if (power == "on") {
-        $("#onButton").css ("visibility", "hidden");
-        $("#offButton").css("visibility", "visible");
-        $("#textDisplay").css("opacity", "1");
-        power = "off";
-
+else if (power == "on") {
+     $("#onButton").css ("visibility", "hidden");
+    $("#offButton").css("visibility", "visible");
+    $("#textDisplay").css("opacity", "0.2");
+    power = "off";
+    oneLife = "off";
+    $("#oneLifeButton").css("background", "red");
+    $("#oneLifeButton").removeClass("fa fa-check");
+    run = false;
+    $("#startButton").css("background", "orange");
+    memory = [];
+    user = [];
+    levelCount = 1;
+    $("#textDisplay").html("--");
+    memoryCount = 0;
+    userCount = 0;
+    matching = true;
+    clearInterval(runningMemory);
+    $(".fourButtons").css("pointer-events", "none");
     }
 });
